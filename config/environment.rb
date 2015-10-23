1baf26cb181df1cfc3ad8db4b929872252581e59
Giorgos Ioannidis <gi00013@surrey.ac.uk>
2015-10-23 23:40:10 +0100

    devise

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.sendgrid.net",
+  :domain               => DOMAIN,
+  :user_name            => ENV['app42949118@heroku.com'],
+  :password             => ENV['6xtdktpt6235'],
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
+ActionMailer::Base.default_url_options = { host: 'onlinevotingsystem.net' }
