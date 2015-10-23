fca1c86316fe5cf69ddede85a2a5652ee0487cc0
Giorgos Ioannidis <gi00013@surrey.ac.uk>
2015-10-23 23:35:22 +0100

    devise

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-:address        => 'smtp.sendgrid.net',
-:port           => '587',
-:authentication => :plain,
-:user_name      => ENV['app42949118@heroku.com'],
-:password       => ENV['6xtdktpt6235'],
-:domain         => 'heroku.com',
-:enable_starttls_auto => true
-}
