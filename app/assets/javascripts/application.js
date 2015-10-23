6d92688d32a2b1837130f865c08b85362247c96d
Giorgos Ioannidis <gi00013@surrey.ac.uk>
2015-10-23 13:29:19 +0100

    create db

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.turbolinks
-//= require turbolinks
 // Loads all Bootstrap javascripts
 //= require bootstrap
 //= require bootstrap-scrollspy
