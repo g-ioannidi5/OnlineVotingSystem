7d714bcf3080efef00d7b219071bc7457e5fe086
Giorgos Ioannidis <gi00013@surrey.ac.uk>
2015-10-24 00:03:26 +0100

    devise

--- config/environment.rb
+++ config/environment.rb
@@ -7,9 +7,9 @@ Rails.application.initialize!
 # Configuration for using SendGrid on Heroku
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-  :user_name => "yourSendGridusernameyougetfromheroku",
-  :password => "yourSendGridpasswordyougetfromheroku",
-  :domain => "staging.freelanceful.com",
+  :user_name => "app42949118@heroku.com",
+  :password => "6xtdktpt6235",
+  :domain => "onlinevotingsystem.herokuapp.com",
   :address => "smtp.sendgrid.net",
   :port => 587,
   :authentication => :plain,
