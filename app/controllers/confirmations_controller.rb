fa6ff5a371c7491e8a9d03521ea95628d313418c
Giorgos Ioannidis <gi00013@surrey.ac.uk>
2015-10-24 01:16:40 +0100

    devise

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,9 +1,23 @@
 class ConfirmationsController < Devise::ConfirmationsController
-
-  private
-
-  def after_confirmation_path_for(resource_name, resource)
-    redirect_to "http://www.onlinevotingsystem.net"
+  def new
+    super
   end
 
+  def create
+    super
+  end
+
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+
+    if resource.errors.empty?
+      set_flash_message(:notice, :confirmed) if is_navigational_format?
+      sign_in(resource_name, resource)
+      respond_with_navigational(resource){  redirect_to "http://www.onlinevotingsystem.net" }
+    else
+      respond_with_navigational(resource.errors, :status => :unprocessable_entity){ render_with_scope :new }
+    end
+  end
 end
+
+
